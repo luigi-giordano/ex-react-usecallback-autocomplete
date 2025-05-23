fd6af729c28df144bdd0fc06c6ccdc6bba1fc0dd
luigi-giordano <luigi.giordano1992@gmail.com>
2025-05-23 11:10:03 +0200

    [milestone 1][no async]

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const [query, setQuery] = useState('');
+  const [productSugg, setProductSugg] = useState([]);
+
+
+  useEffect(() => {
+    if (!query.trim()) {
+      setProductSugg([]);
+      return;
+    }
+    fetch("http://localhost:3333/products")
+      .then(res => res.json())
+      .then(data => {
+        const filtered = data.filter(product =>
+          product.name.toLowerCase().includes(query.toLowerCase())
+        );
+        setProductSugg(filtered);
+      })
+      .catch(error => console.error(error));
+  }, [query]);
+
   return (
     <div>
-      Autocomplete
+      <h1>Autocomplete</h1>
+      <input
+        type="text"
+        placeholder='Cerca prodotto'
+        value={query}
+        onChange={e => setQuery(e.target.value)}
+      />
+      {productSugg.length > 0 && (
+        <div className='dropdown'>
+          {productSugg.map((product) => (
+            <p key={product.id}>{product.name}</p>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
